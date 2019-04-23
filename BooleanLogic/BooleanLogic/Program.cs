31ef1e786588e3974a0416b68a3c0323ce551d3d
feedingcuriosity <kasandrawells@gmail.com>
2019-04-23 12:49:27 -0700

    add BooleanLogic

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// and
+            //Console.WriteLine(true && false); //false because they are not the same
+            //Console.WriteLine(true && true); //true because they are both true
+            //Console.WriteLine(false && false); //false because they are both false
+
+            //// or
+            //Console.WriteLine(true || true); //true because at least one is true
+            //Console.WriteLine(true || false); //true because one is true
+            //Console.WriteLine(false || false); //false because none are true
+
+            //// equal
+            //Console.WriteLine(true == true); //true
+            //Console.WriteLine(true == false); //false
+            //Console.WriteLine(false == false); //true
+
+            //// not equal
+            //Console.WriteLine(true != true); //false
+            //Console.WriteLine(true != false); //true
+            //Console.WriteLine(false != false); //false
+
+            //// x or operator - Evaluates to true if one is true but not both
+            //Console.WriteLine(true ^ true); //false
+            //Console.WriteLine(true ^ false); //true
+            //Console.WriteLine(false ^ false); //false
+
+            //Console.WriteLine(true && true && true || false);
+
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
