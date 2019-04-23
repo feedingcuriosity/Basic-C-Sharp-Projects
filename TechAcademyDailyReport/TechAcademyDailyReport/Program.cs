91b96b26c3566a35b62d190b41a50eeac2f001d6
feedingcuriosity <kasandrawells@gmail.com>
2019-04-23 08:48:51 -0700

    add exercise 57

---
+++ TechAcademyDailyReport/TechAcademyDailyReport/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TechAcademyDailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            int pageNumberInt = Convert.ToInt16(pageNumber);
+
+            Console.WriteLine(@"Do you need help with anything? Please answer ""true"" or ""false"".");
+            string needHelp = Console.ReadLine();
+            bool needHelpBool = Convert.ToBoolean(needHelp);
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string otherFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string studyHours = Console.ReadLine();
+            decimal studyHoursDecimal = Convert.ToDecimal(studyHours);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+            Console.ReadLine();
+       
+        }
+    }
+}
