a3df8ee64e5b74d5d95e682b3ec042a4a53c4f5e
feedingcuriosity <kasandrawells@gmail.com>
2019-05-03 13:55:49 -0700

    add exercise 111

---
+++ ClassAndMethodsExercise111/ClassAndMethodsExercise111/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsExercise111
+{
+    //Create a class
+    class Class1
+    {
+        //create 3 methods
+        //each of which will take on integer parameter in and retrun and integer
+        //the methods do a math operation on the received parameter
+
+        public int Multiply(int userInput)
+        {
+            int result;
+
+            result = userInput * 10;
+            return result;
+        }
+        public int Divide(int userInput)
+        {
+            int result;
+
+            result = userInput / 2;
+            return result;
+        }
+        public int Subtract(int userInput)
+        {
+            int result;
+
+            result = userInput -1 ;
+            return result;
+        }
+    }
+}
---
+++ ClassAndMethodsExercise111/ClassAndMethodsExercise111/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace ClassAndMethodsExercise111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2
+            // ask the user what number thye want to do the math operations on
+            Console.WriteLine("please enter a number you would like to do math operations on?");
+            // get users number
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            //Instantiate object Class1 and assign to variable "m"
+            Class1 m = new Class1();
+            // call each method and pass the user input to the method
+            //method 1 - Multiply
+            Console.WriteLine("First, we will multiply your number by 10:");
+            int multiplyResult = m.Multiply(userInput);
+            Console.WriteLine("Your number multiplied by 10 is "
+                + multiplyResult);
+            Console.ReadLine();
+            //method 2 - Divide
+            Console.WriteLine("Next, we will divide your number by 2:");
+            int divideResult = m.Divide(userInput);
+            Console.WriteLine("Your number divided by 2 is "
+                + divideResult);
+            Console.ReadLine();
+            //method 3 - Subtract
+            Console.WriteLine("First, we will subtract 1 from your number:");
+            int subtractResult = m.Subtract(userInput);
+            Console.WriteLine("Your number minus 1 is "
+                + subtractResult);
+            Console.ReadLine();
+            //display the returned integer to the screen
+        }
+    }
+}
