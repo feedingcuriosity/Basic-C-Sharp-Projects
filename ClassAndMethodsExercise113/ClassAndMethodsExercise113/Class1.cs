d77a60a30dc55e8e389ec9f99e9fd9dc62bc7497
feedingcuriosity <kasandrawells@gmail.com>
2019-05-06 11:50:16 -0700

    add excercise113

---
+++ ClassAndMethodsExercise113/ClassAndMethodsExercise113/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassAndMethodsExercise113
+{
+    //1
+    //Create a class
+    class Class1
+    {
+        //create a void method that takes two integers as parameters.
+        //note: method syntax = <Access Specifier> <Return Type> <Method Name>(Parameter List) {Method Body}
+        public void voidMethod(int integer1, int integer2)
+            {
+            //have the method do a math operation on the first integer
+            int sum = integer1 + 293874;
+            Console.WriteLine("293874 plus {0} is {1}.", integer1, sum);
+            Console.ReadLine();
+
+            //display the second integer to the screen 
+            Console.WriteLine("The second integer is: {0}", integer2);
+            Console.ReadLine();
+        }
+               
+
+    }
+}
---
+++ ClassAndMethodsExercise113/ClassAndMethodsExercise113/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassAndMethodsExercise113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2
+            //in the main() method, instantiate the class
+            //note: instantiating class syntax = <class name> <local class name of choice> = new <class name>();
+            Class1 s = new Class1();
+
+            //3
+            //call the method in the class, passing two numbers
+            //note: calling methods syntax = <local variable name of choice> = <local class name>.<method name>(parameter1 , parameter2);
+            //note 2: but in this case, we are not returning a value in the method because it's void
+            //therefore, it is not assinged a variable for storage.
+            s.voidMethod(20, 30);
+
+            //call the method in the class, specifying the parameters by name
+            s.voidMethod(integer1: 50, integer2: 100);
+        }
+    }
+}
