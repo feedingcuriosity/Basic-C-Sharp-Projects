077561d762916a5b6f93a8d4c4a12d5341092d5a
feedingcuriosity <kasandrawells@gmail.com>
2019-05-06 12:32:33 -0700

    add exercise 114

---
+++ ClassAndMethodsExercise114/ClassAndMethodsExercise114/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ClassAndMethodsExercise114
+{
+    //1
+    //Create a class
+    class Class1
+    {
+        //create a method that will take in an integer
+        public int mathOperation(int integer1)
+        {
+            //do a math operation
+            int quotient = integer1 / 100;
+
+            //return the answer as an integer
+            return quotient;
+        }
+
+
+        //3
+        //create a second method, with the same name, that will take in a decimal
+        //note: this method can have the same name because a different integer type is passed into the method
+        //This means the compiler will know which unique method to call
+        public int mathOperation(decimal decimal1)
+        {
+            //do a different math operation
+            int product = Decimal.ToInt32(decimal1 * 100.0m);
+
+            //return the answer as an integer
+            return product;
+
+        }
+
+        //5
+        //create a third method, with the same name, that will take in a string
+        public int mathOpeartion(string string1)
+        {
+            //convert it to an integer, if possible
+            try
+            {
+                int stringConvert = Convert.ToInt32(string1);
+
+                //do a different math operation
+                int difference = stringConvert - 100;
+
+                //return the answer as an integer
+                return difference;
+            }
+            catch (Exception)
+            {
+                int error = 505;
+                return error;
+            }
+ 
+        }
+
+
+    }
+}
---
+++ ClassAndMethodsExercise114/ClassAndMethodsExercise114/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassAndMethodsExercise114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2
+            //instantiate the class
+            Class1 q = new Class1();
+            //call the one method, passing an integer, such as 12
+            int quotient = q.mathOperation(integer1: 12);
+            //display the result to the screen
+            Console.WriteLine(quotient);
+
+            //4
+            //instantiate the class
+            Class1 p = new Class1();
+            //call the second method, passing in a decimal.
+            int product = p.mathOperation(decimal1: 2.3m);
+            //display the result to the screen
+            Console.WriteLine(product);
+
+            //6
+            //instantiate the class
+            Class1 d = new Class1();
+            //call the third method, passing in a string that equates to an integer.
+            int difference = d.mathOpeartion(string1: "1");
+            //display the result to the screen
+            Console.WriteLine(difference);
+
+            Console.ReadLine();
+        }
+    }
+}
