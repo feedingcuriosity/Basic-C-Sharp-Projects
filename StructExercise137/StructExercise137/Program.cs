2f5174b90772ec3a9b5923037acaff31a6fad1e5
feedingcuriosity <kasandrawells@gmail.com>
2019-05-09 14:14:38 -0700

    add exercise 137

---
+++ StructExercise137/StructExercise137/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExercise137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 12.0m;
+
+            Console.WriteLine("The amount is $" + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
