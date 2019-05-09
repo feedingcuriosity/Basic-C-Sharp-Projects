0e6f800b9ee96124f1ab1b117b1e2469f28becec
feedingcuriosity <kasandrawells@gmail.com>
2019-05-09 09:11:42 -0700

    add exercise 134

---
+++ EnumExercise134/EnumExercise134/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExercise134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //2
+            //prompt the user to enter the current day of the week
+            Console.WriteLine("Please enter the current day of the week:");
+
+            //3
+            //Assign the value to a variable of that enum data type
+            //a - determine userInput as string
+            string userInput = Console.ReadLine().ToLower();
+            //assign Days type to variable for Parse parameter
+            Type dayType = typeof(Days);
+            //4
+            //wrap in a try/catch block
+            try
+            {
+                //b - Parse userInput of string type
+                Days day = (Days)Enum.Parse(dayType, userInput, true);
+                Console.WriteLine("Today is " + day);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
