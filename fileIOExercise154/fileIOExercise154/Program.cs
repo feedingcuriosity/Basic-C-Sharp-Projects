4c839606b52c1cb9eeaa736aa2dbb0c54917779c
feedingcuriosity <kasandrawells@gmail.com>
2019-05-15 08:28:37 -0700

    add exercise 154
    exit

---
+++ fileIOExercise154/fileIOExercise154/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileIOExercise154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Ask user for a number
+            Console.WriteLine("Please provide a number:");
+            //store the userNumber to variable of string datatype
+            string userNumber = (Console.ReadLine() +"\n");
+            //2
+            //log that number to a text file
+            //use streamwriter to append the log
+            using (StreamWriter file = new StreamWriter(@"C:\users\kasan\numberLog.txt", true))
+            {
+                file.WriteLine(userNumber);
+            }
+            //3
+            //print the text file back to the user
+            Console.WriteLine(File.ReadAllText(@"C:\users\kasan\numberLog.txt"));
+            Console.ReadLine();
+
+        }
+    }
+}
